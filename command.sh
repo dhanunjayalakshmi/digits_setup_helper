cab8f2c5370085eb58456a5466d969a5991df707
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2018-05-09 20:23:28 +0530

    message

--- command.sh
+++ command.sh
@@ -1,1 +1,14 @@
-sudo apt-get install --no-install-recommends git graphviz python-dev python-flask python-flaskext.wtf python-gevent python-h5py python-numpy python-pil python-pip python-scipy python-tk
+sudo apt-get install --no-install-recommends build-essential cmake git gfortran libatlas-base-dev libboost-filesystem-dev libboost-python-dev libboost-system-dev libboost-thread-dev libgflags-dev libgoogle-glog-dev libhdf5-serial-dev libleveldb-dev liblmdb-dev libopencv-dev libsnappy-dev python-all-dev python-dev python-h5py python-matplotlib python-numpy python-opencv python-pil python-pip python-pydot python-scipy python-skimage python-sklearn
+
+# example location - can be customized
+export CAFFE_ROOT=~/caffe
+git clone https://github.com/NVIDIA/caffe.git $CAFFE_ROOT -b 'caffe-0.15'
+
+cat $CAFFE_ROOT/python/requirements.txt | xargs -n1 sudo pip install
+
+cd $CAFFE_ROOT
+mkdir build
+cd build
+cmake ..
+make -j"$(nproc)"
+make install
