48a0ce83e26d600079ca1c4d19971edf51ffb363
Dhanunjaya Lakshmi <dhanunjaya.lakshmi60@gmail.com>
2018-05-09 22:38:03 +0530

    message

--- command.sh
+++ command.sh
@@ -1,14 +1,7 @@
-sudo apt-get install --no-install-recommends build-essential cmake git gfortran libatlas-base-dev libboost-filesystem-dev libboost-python-dev libboost-system-dev libboost-thread-dev libgflags-dev libgoogle-glog-dev libhdf5-serial-dev libleveldb-dev liblmdb-dev libopencv-dev libsnappy-dev python-all-dev python-dev python-h5py python-matplotlib python-numpy python-opencv python-pil python-pip python-pydot python-scipy python-skimage python-sklearn
-
 # example location - can be customized
-export CAFFE_ROOT=~/caffe
-git clone https://github.com/NVIDIA/caffe.git $CAFFE_ROOT -b 'caffe-0.15'
+DIGITS_ROOT=~/digits
+git clone https://github.com/NVIDIA/DIGITS.git $DIGITS_ROOT
 
-cat $CAFFE_ROOT/python/requirements.txt | xargs -n1 sudo pip install
+sudo pip install -r $DIGITS_ROOT/requirements.txt
 
-cd $CAFFE_ROOT
-mkdir build
-cd build
-cmake ..
-make -j"$(nproc)"
-make install
+sudo pip install -e $DIGITS_ROOT
